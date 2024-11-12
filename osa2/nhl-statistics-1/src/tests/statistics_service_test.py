03ef56c63dc39b9f34db28dff50aa1aab44dc407
tailkauh <->
2024-11-12 09:46:52 +0200

    init testi

--- osa2/nhl-statistics-1/src/tests/statistics_service_test.py
+++ osa2/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -1,7 +1,24 @@
 import unittest
 from statistics_service import StatisticsService
-from player_reader import PlayerReader
+from player import Player
+
+
+class PlayerReaderStub:
+    
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri", "EDM", 32, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
 
 class TestStatisticsService(unittest.TestCase):
-    def test_example(self):
-        self.assertEqual(0, 0)
+    
+    def setUp(self):
+        self.stats = StatisticsService(PlayerReaderStub())
+
+    def test_init(self):
+        self.assertEqual(hasattr(self.stats, "_players"), True)
